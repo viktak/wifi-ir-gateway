2ed85084b13df87799770d559a872bc7f334748e
viktak <github@viktak.com>
2020-05-29 12:37:24 +0300

    wifi connect fix

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,6 @@ bool needsHeartbeat = false;
 
 //  Other global variables
 config appConfig;
-char timeout = 30;
 bool isAccessPoint = false;
 bool isAccessPointCreated = false;
 TimeChangeRule *tcr;        // Pointer to the time change rule
@@ -1264,14 +1263,14 @@ void loop(){
           // Initialize iteration counter
           char attempt = 0;
 
-          while ((WiFi.status() != WL_CONNECTED) && (attempt++ < timeout)) {
+          while ((WiFi.status() != WL_CONNECTED) && (attempt++ < WIFI_CONNECTION_TIMEOUT)) {
             digitalWrite(CONNECTION_STATUS_LED_GPIO, LOW);
             Serial.print(".");
             delay(50);
             digitalWrite(CONNECTION_STATUS_LED_GPIO, HIGH);
             delay(950);
           }
-          if (attempt > WIFI_CONNECTION_TIMEOUT) {
+          if (attempt >= WIFI_CONNECTION_TIMEOUT) {
             Serial.println();
             Serial.println("Could not connect to WiFi");
             delay(100);
