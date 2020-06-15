3d3c646374f73c047a71430cecd452b10b25f2b6
viktak <github@viktak.com>
2020-06-15 12:35:41 +0300

    mqtt clientID fix

--- src/main.cpp
+++ src/main.cpp
@@ -151,7 +151,7 @@ bool loadSettings(config& data) {
   }
   else
   {
-    strcpy(appConfig.mqttTopic, DEFAULT_MQTT_TOPIC);
+    strcpy(appConfig.mqttTopic, defaultSSID);
   }
   
   if (doc["friendlyName"]){
@@ -225,7 +225,7 @@ void defaultSettings(){
   #endif
 
   appConfig.mqttPort = DEFAULT_MQTT_PORT;
-  strcpy(appConfig.mqttTopic, DEFAULT_MQTT_TOPIC);
+  strcpy(appConfig.mqttTopic, defaultSSID);
 
   appConfig.timeZone = 2;
 
@@ -1346,10 +1346,7 @@ void loop(){
 
         if (!PSclient.connected()) {
           PSclient.setServer(appConfig.mqttServer, appConfig.mqttPort);
-            String clientId = "ESP8266Client-";
-            clientId += String(random(0xffff), HEX);
-
-          if (PSclient.connect(clientId.c_str(), (MQTT_CUSTOMER + String("/") + MQTT_PROJECT + String("/") + appConfig.mqttTopic + "/STATE").c_str(), 0, true, "offline" )){
+          if (PSclient.connect(defaultSSID, (MQTT_CUSTOMER + String("/") + MQTT_PROJECT + String("/") + appConfig.mqttTopic + "/STATE").c_str(), 0, true, "offline" )){
             PSclient.setCallback(mqtt_callback);
 
             PSclient.subscribe((MQTT_CUSTOMER + String("/") + MQTT_PROJECT + String("/") + appConfig.mqttTopic + "/cmnd").c_str(), 0);
