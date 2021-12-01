3e011cca03df44a7450c498f5de624b9583f6dc4
viktak <github@viktak.com>
2021-12-01 09:08:50 +0200

    New IR codes, gitignore updated

--- include/version.h
+++ include/version.h
@@ -7,11 +7,11 @@
 ///////////////////////////////////////////////////////////////
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "368"
+  #define BUILD_NUMBER "393"
 #endif
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION "v1.1.368 - 2021-03-24 18:17:39.898569"
+  #define FIRMWARE_VERSION "v1.1.393 - 2021-09-24 06:41:10.847860"
 #endif
 #ifndef FIRMWARE_VERSION_SHORT
-  #define FIRMWARE_VERSION_SHORT "v1.1.368"
+  #define FIRMWARE_VERSION_SHORT "v1.1.393"
 #endif
