51ca40f0e45aac7486c4af117357473d5d5aff5f
viktak <github@viktak.com>
2022-03-13 11:09:37 +0200

    license corrected

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ Pull requests are welcome. For major changes, please open an issue first to disc
 Please make sure to update tests as appropriate.
 
 ## License
-[Attribution-ShareAlike 4.0 International](https://creativecommons.org/licenses/by-sa/4.0/)
+[Attribution 4.0 International](https://creativecommons.org/licenses/by/4.0/)
--- include/version.h
+++ include/version.h
@@ -7,11 +7,11 @@
 ///////////////////////////////////////////////////////////////
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "118"
+  #define BUILD_NUMBER "120"
 #endif
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION "v1.2.118 - 2022-03-13 10:53:48.326572"
+  #define FIRMWARE_VERSION "v1.2.120 - 2022-03-13 11:06:41.378921"
 #endif
 #ifndef FIRMWARE_VERSION_SHORT
-  #define FIRMWARE_VERSION_SHORT "v1.2.118"
+  #define FIRMWARE_VERSION_SHORT "v1.2.120"
 #endif
